84eec85657a8206239d0b753b2f34cca5a3ac27a
BLUE KG <109420506+tarunmasterbraingmailcom@users.noreply.github.com>
2026-07-24 12:31:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -377,8 +377,6 @@ function animateFireworks(){
 animateFireworks();
 // ===== CELEBRATE BUTTON =====
 
-const celebrateBtn = document.getElementById("celebrate");
-
 if (celebrateBtn) {
 
     celebrateBtn.addEventListener("click", () => {
